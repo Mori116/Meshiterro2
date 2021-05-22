7c36efda03019e7ee63cf94dd9555a0bb33dcbaa
Mori116 <cacaomame096@gmail.com>
2021-05-22 12:03:13 +0000

    [Finish]画像投稿機能の追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'de7a147dc65df076fadc1826c519e1310cee885ecfa3b54b5731b041867c8c8b5374da58407ba7b2af65e46dd41e4219803f95ea694043435430d00a195d6858'
