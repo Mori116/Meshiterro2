16f02aa44dcf34847d2ce6ab474a134e8b1b2f95
Mori116 <cacaomame096@gmail.com>
2021-05-23 06:39:31 +0000

    [Finish]4-5 ユーザ詳細ページ、プロフィール編集機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'homes#top'
+devise_for :users
+root to: 'homes#top'
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resource :favorites, only: [:create, :destroy]
-    # idが不要な場合、単数形で記述する
-    resources :post_comments, only: [:create, :destroy]
-  end
+resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+  resource :favorites, only: [:create, :destroy]
+  # idが不要な場合、単数形で記述する
+  resources :post_comments, only: [:create, :destroy]
+end
+
+resources :users, only: [:show, :edit, :update]
 
 end
